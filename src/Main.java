42d0c21ec0ee46af9ab65e09fd9850667c1577db
Laboratório Informática <labinfo@unifil.br>
2019-11-01 21:33:05 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+
+    //interpolação polinomial
+    //entrada para digitar cinco pontos (pares ordenados)
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
